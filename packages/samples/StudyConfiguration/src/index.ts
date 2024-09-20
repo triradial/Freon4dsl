ff80bac65bf92ee66343fc51d5c723a24a57ed44
mike.vogel <globallyunique@gmail.com>
2024-09-20 13:32:01 -0400

    commented out server changes

--- packages/samples/StudyConfiguration/src/index.ts
+++ packages/samples/StudyConfiguration/src/index.ts
@@ -1,6 +1,7 @@
 export { StudyConfigurationModelEnvironment } from "./config/gen/StudyConfigurationModelEnvironment";
-// Change to support editor initializing language MV 
 export { StudyConfigurationModel, StudyConfiguration, Period, Task, Event, Description } from "./language/gen";
 export {extendToSupportSmartDuplication} from "./custom/extensions/ExtensionLib";
-// export { Simulator } from "./custom/timeline/Simulator";
-// export { TimelineScriptTemplate } from "./custom/templates/TimelineScriptTemplate";
+export { Simulator } from "./custom/timeline/Simulator";
+export { TimelineTableTemplate } from "./custom/templates/TimelineTableTemplate";
+export { TimelineChartTemplate } from "./custom/templates/TimelineChartTemplate";
+export { StudyChecklistDocumentTemplate } from "./custom/templates/StudyChecklistDocumentTemplate";
