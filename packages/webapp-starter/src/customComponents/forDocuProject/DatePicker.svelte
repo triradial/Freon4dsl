780263e6bd6e8e4aff47bcfde0f2b24c5d147427
mike.vogel <globallyunique@gmail.com>
2024-10-08 14:14:39 -0400

    fixed datepicker css missing }

--- packages/webapp-starter/src/customComponents/forDocuProject/DatePicker.svelte
+++ packages/webapp-starter/src/customComponents/forDocuProject/DatePicker.svelte
@@ -96,13 +96,12 @@
         padding: 0.625rem; /* 10px */
         color-scheme: dark;
     }
-.datepicker-input:hover {
-
-
-    .datepicker-input:focus {
-        --tw-ring-color: rgb(59 130 246);
-        border-color: rgb(59 130 246);
-        color: rgb(255, 255, 255);
-        border-width: 1px;
+    .datepicker-input:hover {
+        .datepicker-input:focus {
+            --tw-ring-color: rgb(59 130 246);
+            border-color: rgb(59 130 246);
+            color: rgb(255, 255, 255);
+            border-width: 1px;
+        }
     }
 </style>
