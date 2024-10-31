3fa5f63ef0b42867262c339482364e8c83828364
graham mcgibbon <graham.mcgibbon@triradial.com>
2024-10-30 22:23:20 -0400

    fix for cross-env in server and port

--- packages/server/src/server/server-starter.ts
+++ packages/server/src/server/server-starter.ts
@@ -31,7 +31,7 @@ router.get('/health', async (ctx) => {
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-const port = process.env.PORT || 8001;
+const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
