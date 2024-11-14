fa02ba61a86fb91eb8fed99d1290a1a723a0e28f
mike.vogel <globallyunique@gmail.com>
2024-11-14 17:15:11 -0500

    start of validator

--- packages/samples/StudyConfiguration/src/validator/CustomStudyConfigurationModelValidator.ts
+++ packages/samples/StudyConfiguration/src/validator/CustomStudyConfigurationModelValidator.ts
@@ -2,7 +2,29 @@
 import { FreError, FreErrorSeverity } from "@freon4dsl/core";
 import { StudyConfigurationModelDefaultWorker } from "../utils/gen/StudyConfigurationModelDefaultWorker.js";
 import { StudyConfigurationModelCheckerInterface } from "./gen/StudyConfigurationModelValidator.js";
+import { Event } from "../language/gen/index.js";
 
 export class CustomStudyConfigurationModelValidator extends StudyConfigurationModelDefaultWorker implements StudyConfigurationModelCheckerInterface {
     errorList: FreError[] = [];
+
+    public execBeforeEvent(_modelelement: Event): boolean {
+        console.log(" execBeforeEvent calling super.execBeforeEvent");
+        let result = super.execBeforeEvent(_modelelement);
+        console.log("entered execBeforeEvent result:", result);
+        console.log("execBeforeEvent before finding error", this.errorList.length);
+
+        const errorIndex = this.errorList.indexOf(
+            this.errorList.find((error) => {
+                console.log("execBeforeEvent error.message", error.message);
+                return error.message === "Property 'alternativeName' must have a value";
+            }),
+        );
+        console.log("execBeforeEvent errorIndex", errorIndex);
+        if (errorIndex !== -1) {
+            console.log("execBeforeEvent removing error");
+            this.errorList.splice(errorIndex, 1);
+            console.log("execBeforeEvent errorList", this.errorList.length);
+        }
+        return false;
+    }
 }
