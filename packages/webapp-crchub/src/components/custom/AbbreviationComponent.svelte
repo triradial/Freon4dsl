28c9f22972c601f0bba07eaafa89151a9ec361f2
gmcgibbon-triradial <graham.mcgibbon@triradial.com>
2025-10-01 13:07:45 -0400

    fix abbreviation text

--- packages/webapp-crchub/src/components/custom/AbbreviationComponent.svelte
+++ packages/webapp-crchub/src/components/custom/AbbreviationComponent.svelte
@@ -46,6 +46,6 @@
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
-<#if abbreviatedText()>
+{#if abbreviatedText()}
 <div id="{id}" class="abbreviation-text">{abbreviatedText()}</div>
-</#if>
+{/if}
