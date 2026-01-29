285b69cf9535532ddffac445fcf000cec37c2e23
mike.vogel <globallyunique@gmail.com>
2026-01-29 11:11:58 -0500

    removed islr language, webapp, server from build

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -1,9 +1,8 @@
-import { defineWorkspace } from 'vitest/config'
+import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
   // Reference individual package configs
   './packages/languages/study-configuration/vitest.config.ts',
-  './packages/languages/project-configuration/vitest.config.ts',
   './packages/webapp-crchub/vitest.config.ts',
   './packages/meta/src/vitest.config.ts',
   // Inline configs for packages without explicit config files
@@ -21,16 +20,6 @@ export default defineWorkspace([
     test: {
       name: 'server-crchub',
       root: './packages/server-crchub',
-      include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-      exclude: ['**/node_modules/**', '**/dist/**'],
-      globals: true,
-      environment: 'node',
-    },
-  },
-  {
-    test: {
-      name: 'server-islr',
-      root: './packages/server-islr',
       include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
       exclude: ['**/node_modules/**', '**/dist/**'],
       globals: true,
